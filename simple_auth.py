55ac6383d98f3699405249fb5f46774a7490e582
Zemphix <vafinaaa51@gmail.com>
2025-09-04 00:34:15 +0500

    added auth system

---
+++ simple_auth.py
@@ -0,0 +1,22 @@
+import hashlib
+import os
+
+def hash_password(password: str) -> tuple[str, str]:
+    salt = os.urandom(32)
+    hashed_password = hashlib.sha256(salt + password.encode()).hexdigest()
+    return hashed_password, salt.hex()
+
+
+def authorization(password: str, salt: str, hashed_password: str) -> bool:
+    result = hashlib.sha256(bytes.fromhex(salt) + password.encode()).hexdigest()
+    if result == hashed_password:
+        print('Correct password!')
+        return True
+    else:
+        print('Incorrect password')
+        return False
+
+password = "some_password"
+password_hash, salt = hash_password(password)
+
+authorization(password, salt, password_hash)
